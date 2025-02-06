ad213fd67910354d7d6af079ed82e563613e5002
Rohith7124 <jittarohithreddy18@gmail.com>
2025-02-06 11:40:13 +0530

    Add files via upload

---
+++ Demo060.java
@@ -0,0 +1,13 @@
+public class Demo060 {
+    public static void main(String[] args) {
+        int n = 20, first = 0, second = 1;
+
+        System.out.print("Fibonacci Series: " + first + ", " + second);
+        for (int i = 2; i < n; i++) {
+            int next = first + second;
+            System.out.print(", " + next);
+            first = second;
+            second = next;
+        }
+    }
+}
